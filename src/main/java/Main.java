d66529641761044851e236c32fa4ced0e856f2e8
italobruhhh <169067935+italobruhhh@users.noreply.github.com>
2024-07-04 10:56:29 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.println("olá, diga seu nome");
+    String nome = sc.nextLine();
+    System.out.printf("e sua idade");
+    int  idade = sc.nextInt();
+    
+    
+  }
+
+
+}
